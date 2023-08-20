5cb98a077a50a642c28ceae1ff193d6b73faf2bd
Rafael <queirozrafael348@gmail.com>
2023-08-20 14:30:16 -0300

    correção de rotas

--- src/app/models/Group.js
+++ src/app/models/Group.js
@@ -1,53 +1,50 @@
 import { Model, Sequelize } from 'sequelize';
 
 class Group extends Model {
-  static init(sequelize) {
-    super.init(
-      {
-        name: Sequelize.STRING,
-        is_private: Sequelize.BOOLEAN,
-      },
-      {
-        sequelize,
-        tableName: 'groups',
-      }
-    );
+    static init(sequelize) {
+        super.init({
+            group_name: Sequelize.STRING,
+            is_private: Sequelize.BOOLEAN,
+        }, {
+            sequelize,
+            tableName: 'groups',
+        });
 
-    return this;
-  }
+        return this;
+    }
 
-  static associate(models) {
-    this.belongsTo(models.User, {
-      foreignKey: 'owner_id',
-      as: 'administrator',
-    });
+    static associate(models) {
+        this.belongsTo(models.User, {
+            foreignKey: 'owner_id',
+            as: 'administrator',
+        });
 
-    this.hasMany(models.Topic, { foreignKey: 'group_id', as: 'topics' });
+        this.hasMany(models.Topic, { foreignKey: 'group_id', as: 'topics' });
 
-    this.belongsToMany(models.User, {
-      foreignKey: 'group_id',
-      through: 'groups_members',
-      as: 'members',
-    });
+        this.belongsToMany(models.User, {
+            foreignKey: 'group_id',
+            through: 'groups_members',
+            as: 'members',
+        });
 
-    this.belongsToMany(models.User, {
-      foreignKey: 'group_id',
-      through: 'groups_bans',
-      as: 'bans',
-    });
+        this.belongsToMany(models.User, {
+            foreignKey: 'group_id',
+            through: 'groups_bans',
+            as: 'bans',
+        });
 
-    this.belongsToMany(models.User, {
-      foreignKey: 'group_id',
-      through: 'groups_moderators',
-      as: 'moderators',
-    });
+        this.belongsToMany(models.User, {
+            foreignKey: 'group_id',
+            through: 'groups_moderators',
+            as: 'moderators',
+        });
 
-    this.belongsToMany(models.User, {
-      foreignKey: 'group_id',
-      through: 'request_entrys',
-      as: 'requesters',
-    });
-  }
+        this.belongsToMany(models.User, {
+            foreignKey: 'group_id',
+            through: 'request_entrys',
+            as: 'requesters',
+        });
+    }
 }
 
 export default Group;
--- src/app/models/Topic.js
+++ src/app/models/Topic.js
@@ -1,28 +1,25 @@
 import { Model, Sequelize } from 'sequelize';
 
 class Topic extends Model {
-  static init(sequelize) {
-    super.init(
-      {
-        name: Sequelize.STRING,
-        is_closed: Sequelize.BOOLEAN,
-      },
-      {
-        sequelize,
-        tableName: 'topics',
-      }
-    );
+    static init(sequelize) {
+        super.init({
+            topic_title: Sequelize.STRING,
+            is_closed: Sequelize.BOOLEAN,
+        }, {
+            sequelize,
+            tableName: 'topics',
+        });
 
-    return this;
-  }
+        return this;
+    }
 
-  static associate(models) {
-    this.belongsTo(models.User, { foreignKey: 'author_id', as: 'author' });
+    static associate(models) {
+        this.belongsTo(models.User, { foreignKey: 'author_id', as: 'author' });
 
-    this.belongsTo(models.Group, { foreignKey: 'group_id', as: 'group' });
+        this.belongsTo(models.Group, { foreignKey: 'group_id', as: 'group' });
 
-    this.hasMany(models.Comment, { foreignKey: 'author_id', as: 'comments' });
-  }
+        this.hasMany(models.Comment, { foreignKey: 'author_id', as: 'comments' });
+    }
 }
 
 export default Topic;
